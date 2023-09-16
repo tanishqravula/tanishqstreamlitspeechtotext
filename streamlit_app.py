843ac6c539b5ce7b0ab5a8386498979de5d8c13a
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-09-16 18:27:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if f is not None:
         bytes_data = f.getvalue()
 
         # Load your API key from an environment variable or secret management service
-        api_token = st.secrets[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]
+        api_token = st.secrets["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"]
 
         # endregion API key
         headers = {"Authorization": f"Bearer {api_token}"}
